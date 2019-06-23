bf4cb1af33308a6db23d914a50811ebcdb78bfdb
Philip B <philip.blaquiere@4mation.com.au>
2019-06-23 19:07:18 +1000

    using ruleset and rankingset services in checkpasswordservice

--- PasswordCheck.Services/CheckPasswordService.cs
+++ PasswordCheck.Services/CheckPasswordService.cs
@@ -1,61 +1,73 @@
-﻿using HaveIBeenPND;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PasswordCheck.Contracts;
+using PasswordCheck.Data;
+using PasswordCheck.Services.Contracts;
+using PasswordCheck.Services.Data;
+using HaveIBeenPND;
 using HaveIBeenPND.Contracts;
 using HaveIBeenPND.Entities;
-using PasswordCheck.Data;
-using PasswordCheck.Services.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordCheck.Services
 {
-	public class CheckPasswordService : ICheckPasswordService
-	{
-		public async Task<CheckPasswordResult> Check(
-			string password,
-			string ruleSetName,
-			string rankingSetName,
-			bool isCheckHaveIBeenPwned)
-		{
-			RuleSet ruleSet = RuleSetProvider.GetRuleSet(ruleSetName); 
+    public class CheckPasswordService : ICheckPasswordService
+    {
+        private readonly IRuleSetService ruleSetService;
+        private readonly IRankingSetService rankingSetService;
+        private readonly IScoreNormalizer scoreNormalizer;
+        private IHaveIBeenPNDService pndService;
 
-			// Test Password
-			RuleSetTestResult ruleSetTestResults = ruleSet.Test(password);
+        public CheckPasswordService(
+            IRuleSetService ruleSetService = null,
+            IRankingSetService rankingSetService = null,
+            IScoreNormalizer scoreNormalizer = null,
+            IHaveIBeenPNDService pndService = null)
+        {
+            this.ruleSetService = ruleSetService ?? new RuleSetService();
+            this.rankingSetService = rankingSetService ?? new RankingSetService();
+            this.scoreNormalizer = scoreNormalizer ?? new ScoreNormalizer();
+            this.pndService = pndService ?? new HaveIBeenPNDService();
+        }
 
-			ScoreNormalizer scoreInterpreter = new ScoreNormalizer();
+        public async Task<CheckPasswordResult> Check(
+            string password,
+            string ruleSetName,
+            string rankingSetName,
+            bool isCheckHaveIBeenPwned)
+        {
+            RuleSet ruleSet = await this.ruleSetService.GetRuleSet(ruleSetName);
+            RankingSet rankingSet = await this.rankingSetService.GetRankingSet(rankingSetName);
 
-			RankingSet rankingSet = RankingSetProvider.GetRanking(rankingSetName);
+            // Test Password
+            RuleSetTestResult ruleSetTestResults = ruleSet.Test(password);
 
-			// Normalize Score
-			int normalizedScore = scoreInterpreter.Normalize(
-				ruleSetTestResults.IsSuccess ? ruleSetTestResults : null,
-				ruleSet,
-				rankingSet);
+            // Normalize Score
+            int normalizedScore = this.scoreNormalizer.Normalize(
+                ruleSetTestResults.IsSuccess ? ruleSetTestResults : null,
+                ruleSet,
+                rankingSet);
 
-			// Interpret
-			KeyValuePair<int, string>? interpretedRanking = rankingSet.GetClosestRanking(normalizedScore);
+            // Interpret
+            KeyValuePair<int, string>? interpretedRanking = rankingSet.GetClosestRanking(normalizedScore);
 
-			PNDPassword pndPassword = null;
+            PNDPassword pndPassword = null;
 
-			if (isCheckHaveIBeenPwned)
-			{
-				// Check if pass has been pwned on the external website
-				IHaveIBeenPNDService pndService = new HaveIBeenPNDService();
+            if (isCheckHaveIBeenPwned)
+            {
+                // Check if pass has been pwned on the external website
+                pndPassword = await this.pndService.HaveIBeenPND(password);
+            }
 
-				pndPassword = await pndService.HaveIBeenPND(password);
-			}
-
-			return new CheckPasswordResult(
-				score: interpretedRanking?.Key,
-				ranking: interpretedRanking?.Value,
-				rankingSetName: rankingSet?.Name,
-				ruleSet: ruleSet,
-				isSuccess: ruleSetTestResults.IsSuccess,
-				pndPassword: pndPassword,
-				rulesPassed: ruleSetTestResults.RulesPassed,
-				rulesFailed: ruleSetTestResults.RulesFailed,
-				rulesRecommendations: ruleSetTestResults.RulesRecommendations);
-		}
-	}
+            return new CheckPasswordResult(
+                score: interpretedRanking?.Key,
+                ranking: interpretedRanking?.Value,
+                rankingSetName: rankingSet?.Name,
+                ruleSet: ruleSet,
+                isSuccess: ruleSetTestResults.IsSuccess,
+                pndPassword: pndPassword,
+                rulesPassed: ruleSetTestResults.RulesPassed,
+                rulesFailed: ruleSetTestResults.RulesFailed,
+                rulesRecommendations: ruleSetTestResults.RulesRecommendations);
+        }
+    }
 }
--- PasswordCheck.Services/RuleSetService.cs
+++ PasswordCheck.Services/RuleSetService.cs
@@ -5,19 +5,19 @@ using PasswordCheck.Services.Contracts;
 
 namespace PasswordCheck.Services
 {
-	public class RuleSetService : IRuleSetService
-	{
-		public Task<RuleSet> GetRuleSet(string ruleSetName)
-		{
-			return Task.Run(() => RuleSetProvider.GetRuleSet(ruleSetName));
-		}
+    public class RuleSetService : IRuleSetService
+    {
+        public Task<RuleSet> GetRuleSet(string ruleSetName)
+        {
+            return Task.Run(() => RuleSetProvider.GetRuleSet(ruleSetName));
+        }
 
-		public Task<string[]> GetRuleSets()
-		{
-			return Task.Run(() => RuleSetProvider
-				.RuleSets
-				.Select(ruleSet => $"{ruleSet.Name}")
-				.ToArray());
-		}
-	}
+        public Task<string[]> GetRuleSets()
+        {
+            return Task.Run(() => RuleSetProvider
+                .RuleSets
+                .Select(ruleSet => $"{ruleSet.Name}")
+                .ToArray());
+        }
+    }
 }
