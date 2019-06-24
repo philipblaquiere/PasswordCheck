b4c72de913d9b6fb5e6132f90835cd4c19cb705d
Philip B <philip.blaquiere@4mation.com.au>
2019-06-24 11:39:25 +1000

    move extensions to folder

---
+++ PasswordCheck.Services/Data/Extensions/RankingSetExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordCheck.Data
+{
+    public static class RankingSetExtensions
+    {
+        public static int GetMaxScore(this RankingSet rankingSet)
+        {
+            return rankingSet
+                .Rankings
+                .Select(ranking => ranking.Key)
+                .Max();
+        }
+
+        /// <summary>
+        /// Gets the closest score in the ranking, such that a non-existant score
+        /// in rankings will return a score in nearest-low basis
+        /// </summary>
+        /// <param name="normalizedScore"></param>
+        /// <param name="rankingSet"></param>
+        /// <returns></returns>
+        public static KeyValuePair<int, string> GetClosestRanking(
+            this RankingSet rankingSet,
+            int normalizedScore)
+        {
+            var equivalentScore = 0;
+
+            foreach (var ranking in rankingSet.Rankings.OrderBy(ranking => ranking.Key))
+            {
+                if (normalizedScore >= ranking.Key)
+                {
+                    equivalentScore = ranking.Key;
+                }
+            }
+
+            return rankingSet.Rankings.FirstOrDefault(ranking => ranking.Key == equivalentScore);
+        }
+    }
+}
